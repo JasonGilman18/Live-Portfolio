00084dce449b6c68630b1bd15b1adf756d1228e0
JasonGilman18 <jasongilman18@gmail.com>
2020-12-15 12:37:43 -0600

    mobile layout fix

--- src/pages/desktop.tsx
+++ src/pages/desktop.tsx
@@ -14,20 +14,20 @@ export const Desktop: React.FunctionComponent = () => (
 
     <Container fluid className="welcomeContainer">
           <Row className="welcomeRow">
-            <Col xl={8} lg={8} md={8}>
+            <Col xl={8} lg={8} md={8} sm={8}>
               <StyledWelcomeDesktop main={true}>
                 <h1 className="welcomeHeading">Welcome</h1>
                 <h2 className="welcomeSubHeading">Jason Gilman's Portfolio</h2>
               </StyledWelcomeDesktop>
             </Col>
-            <Col xl={4} lg={4} md={4}>
+            <Col xl={4} lg={4} md={4} sm={4}>
               <Container className="linkBox">
                   <LinkTable links={["Link 1", "Link 2", "Link 3", "Link 4"]}></LinkTable>
               </Container>
             </Col>
           </Row>
           <Row className="introRow">
-            <Col xl={8} lg={8} md={8}>
+            <Col xl={8} lg={8} md={8} sm={8}>
               <Container className="skills">
                 <h2 className="skillLabel">Software Engineering</h2>
                 <div className="skillDivider"/>
@@ -43,7 +43,7 @@ export const Desktop: React.FunctionComponent = () => (
                 <div className="skillUnderline three"/>
               </Container>
             </Col>
-            <Col xl={4} lg={4} md={4}>
+            <Col xl={4} lg={4} md={4} sm={4}>
               <StyledWelcomeDesktop main={false}>
                 <Github className="icon"/>
                 <Linkedin className="icon"/>
